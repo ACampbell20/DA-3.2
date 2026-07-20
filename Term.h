1a534f80557a2520514f8f6c0256a7c97f61b9bd
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-20 12:58:01 -0500

    Add Term class definition with basic operations

--- Term.h
+++ Term.h
@@ -1,1 +1,35 @@
+#ifndef TERM_H
+#define TERM_H
 
+using namespace std;
+
+class Term
+{
+private:
+    double coefficient;
+    char variable;
+    int exponent;
+
+public:
+    Term();
+    Term(double coeff, char var = 'x', int exp = 0);
+    Term(const Term& other);
+
+    double getCoefficient() const;
+    char getVariable() const;
+    int getExponent() const;
+
+    void setCoefficient(double coeff);
+    void setVariable(char var);
+    void setExponent(int exp);
+
+    Term& operator=(const Term& other);
+
+    Term operator+(const Term& other) const;
+    Term operator-(const Term& other) const;
+    Term operator*(const Term& other) const;
+
+    bool operator==(const Term& other) const;
+};
+
+#endif
