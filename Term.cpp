ec783f86d95181685c924d44b5c8a2e8b9ee9783
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-21 21:05:02 -0500

    Implement Term class with basic operations

--- Term.cpp
+++ Term.cpp
@@ -1,5 +1,96 @@
 #include "Term.h"
-#include <string>
 
-using namespace std;
+Term::Term()
+{
+    coefficient = 0;
+    variable = 'x';
+    exponent = 0;
+}
 
+Term::Term(double coeff, char var, int exp)
+{
+    coefficient = coeff;
+    variable = var;
+    exponent = exp;
+}
+
+Term::Term(const Term& other)
+{
+    coefficient = other.coefficient;
+    variable = other.variable;
+    exponent = other.exponent;
+}
+
+double Term::getCoefficient() const
+{
+    return coefficient;
+}
+
+char Term::getVariable() const
+{
+    return variable;
+}
+
+int Term::getExponent() const
+{
+    return exponent;
+}
+
+void Term::setCoefficient(double coeff)
+{
+    coefficient = coeff;
+}
+
+void Term::setVariable(char var)
+{
+    variable = var;
+}
+
+void Term::setExponent(int exp)
+{
+    exponent = exp;
+}
+
+Term& Term::operator=(const Term& other)
+{
+    if (this != &other)
+    {
+        coefficient = other.coefficient;
+        variable = other.variable;
+        exponent = other.exponent;
+    }
+
+    return *this;
+}
+
+Term Term::operator+(const Term& other) const
+{
+    if (exponent != other.exponent || variable != other.variable)
+        return *this;
+
+    return Term(coefficient + other.coefficient, variable, exponent);
+}
+
+Term Term::operator-(const Term& other) const
+{
+    if (exponent != other.exponent || variable != other.variable)
+        return *this;
+
+    return Term(coefficient - other.coefficient, variable, exponent);
+}
+
+Term Term::operator*(const Term& other) const
+{
+    return Term(
+        coefficient * other.coefficient,
+        variable,
+        exponent + other.exponent
+    );
+}
+
+bool Term::operator==(const Term& other) const
+{
+    return coefficient == other.coefficient &&
+           variable == other.variable &&
+           exponent == other.exponent;
+}
