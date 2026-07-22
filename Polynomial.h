e08dc0c5893e4552e1d3dc5789e8ed74ca40bb33
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-21 21:06:27 -0500

    Add Polynomial class definition with operators

--- Polynomial.h
+++ Polynomial.h
@@ -1,1 +1,43 @@
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
 
+#include "Term.h"
+#include <iostream>
+
+using namespace std;
+
+class Polynomial
+{
+private:
+    Term* terms;
+    int numberOfTerms;
+
+public:
+
+    Polynomial();
+    Polynomial(Term terms[], int size);
+    Polynomial(const Polynomial& other);
+
+
+    ~Polynomial();
+
+
+    Term* getTerms() const;
+    int getNumberOfTerms() const;
+
+    void setTerms(Term terms[], int size);
+    void setNumberOfTerms(int size);
+
+
+    Polynomial& operator=(const Polynomial& other);
+
+    Polynomial operator+(const Polynomial& other) const;
+    Polynomial operator-(const Polynomial& other) const;
+    Polynomial operator*(const Polynomial& other) const;
+
+    bool operator==(const Polynomial& other) const;
+
+    friend ostream& operator<<(ostream& out, const Polynomial& poly);
+};
+
+#endif
